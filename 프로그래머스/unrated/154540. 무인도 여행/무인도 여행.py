809a6eaf82ba731e5f82b534197fc18d23edcdea
Jihoon <1204jh@skku.edu>
2023-03-07 17:08:44 +0900

    [unrated] Title: 무인도 여행, Time: 0.42 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+def dfs(x, y, visited, maps):
+    
+    if visited[x][y]:
+        return
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    queue = deque()
+    cnt = int(maps[x][y])
+    visited[x][y] = True
+    queue.append((x, y))
+    
+    while queue:
+        x, y = queue.popleft()
+        for _ in range(4):
+            nx = x + dx[_]
+            ny = y + dy[_]
+            if 0 <= nx and nx < len(maps) and 0 <= ny and ny < len(maps[0]) and not visited[nx][ny]:
+                cnt += int(maps[nx][ny])
+                visited[nx][ny] = True
+                queue.append((nx, ny))
+    return cnt
+
+def solution(maps):
+    answer = []
+    visited = [[n.isalpha() for n in r] for r in maps]
+    
+    for i1, r in enumerate(maps):
+        for i2, c in enumerate(r):
+            if c != 'X':
+                answer.append(dfs(i1, i2, visited, maps))
+
+    if not answer:
+        return [-1]
+    
+    answer = [n for n in answer if str(n).isdigit()]
+    answer = sorted(answer)
+
+    return answer
